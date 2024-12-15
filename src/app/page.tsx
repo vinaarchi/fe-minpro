209e6aa587aabf8c6143f049310188835258b114
kusumaningrum-code <kusumaningrum416@gmail.com>
2024-12-15 20:41:24 +0700

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/Footer";
 import Footer2 from "@/components/Footer2";
+import Navbar from "@/components/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <Footer />
       <Footer2 />
     </div>
