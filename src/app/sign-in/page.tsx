c7fc0bdaa575d3d49a77b28b5135bc1a2db1513e
vinaarchi <vinaa.as4@gmail.com>
2024-12-16 14:11:42 +0700

    sementara editing

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -13,13 +13,6 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 const signIn = () => {
   return (
