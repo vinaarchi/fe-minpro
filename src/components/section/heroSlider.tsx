57bbf63d193034caa22c2dfe54f4eed6672358c1
vinaarchi <vinaa.as4@gmail.com>
2024-12-17 15:00:05 +0700

    ini bagian heronya, masih akan di editing

--- src/components/section/heroSlider.tsx
+++ src/components/section/heroSlider.tsx
@@ -2,11 +2,60 @@
 
 import Image from "next/image";
 import * as React from "react";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselContent,
+  CarouselPrevious,
+  CarouselNext,
+} from "../ui/carousel";
+import { Card } from "../ui/card";
 
 const Hero = () => {
   return (
     <div className="m-10 p-5">
-      <p>CTH 1</p>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>
+            <Image
+              src="/images/keshi.png"
+              alt="Banner1"
+              width={1000}
+              height={1000}
+              className="rounded-md"
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src="/images/yoasobi.png"
+              alt="Banner2"
+              width={1000}
+              height={1000}
+              className="rounded-md"
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src="/images/conct2.jpg"
+              alt="Banner3"
+              width={2000}
+              height={1000}
+              className="rounded-md"
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <Image
+              src="/images/swara.png"
+              alt="Banner3"
+              width={900}
+              height={1000}
+              className="rounded-md"
+            />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
