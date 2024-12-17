cb97bbbf6cb585dafdf7f766a3b428991b05841a
vinaarchi <vinaa.as4@gmail.com>
2024-12-17 11:20:28 +0700

    install carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/Footer";
 import Footer2 from "@/components/Footer2";
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/section/heroSlider";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
       <Footer />
       <Footer2 />
     </div>
--- src/components/section/heroSlider.tsx
+++ src/components/section/heroSlider.tsx
@@ -1,4 +1,16 @@
 "use client"
 
+import Image from "next/image"
 import * as React from "react"
 import Slider from "react-slick"
+
+const Hero = () => {
+    
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default Hero;
