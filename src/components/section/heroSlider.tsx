1dd3f592b1ee282b56bbda1fc922f58ba275511d
vinaarchi <vinaa.as4@gmail.com>
2024-12-17 14:10:04 +0700

    install carousel

--- src/components/section/heroSlider.tsx
+++ src/components/section/heroSlider.tsx
@@ -1,16 +1,36 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import * as React from "react"
-import Slider from "react-slick"
+import Image from "next/image";
+import * as React from "react";
+import Slider from "react-slick";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { Card, CardContent } from "../ui/card";
 
 const Hero = () => {
-    
-    return (
-        <div>
-           
-        </div>
-    )
-}
+  return (
+    <div className="m-10 p-5">
+      <Carousel>
+        <Card>
+          <CardContent className="flex aspect-auto items-center justify-center p-44">
+            <Image
+              src="/images/lany.jpg"
+              alt="Banner1"
+              layout="fill"
+              objectFit="cover"
+            />
+          </CardContent>
+        </Card>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
 
 export default Hero;
