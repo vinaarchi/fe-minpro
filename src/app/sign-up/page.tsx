879995cea3bd9138361eef09032a8aff9a6bb4e5
Avina Anggraeni <vinaa.as4@gmail.com>
2024-12-17 21:57:40 +0700

    Register login (#12)
    * signup-in
    
    * contoh
    
    * register-login-vina
    
    * nambahin interface signin
    
    * pembaharuan register login -vina
    
    * pembeharuan lagi
    
    * nyobain integrasi dengan backend

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -25,7 +25,7 @@ interface FormValue {
 const signUp: React.FunctionComponent<ISignUpPageProps> = (props) => {
   const onSignUp = async (values: FormValue) => {
     try {
-      const res = await callAPI.post("/user/register", {
+      const res = await callAPI.post("/user/sign-up", {
         fullname: values.fullname,
         username: values.username,
         email: values.email,
