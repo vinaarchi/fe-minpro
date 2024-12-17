9a3d613bf727ef192b1256fdd66926697cdc7df1
vinaarchi <vinaa.as4@gmail.com>
2024-12-17 14:31:23 +0700

    cuma contoh

--- src/components/section/heroSlider.tsx
+++ src/components/section/heroSlider.tsx
@@ -3,13 +3,10 @@
 import Image from "next/image";
 import * as React from "react";
 
-
-
-
 const Hero = () => {
   return (
     <div className="m-10 p-5">
-      
+      <p>CTH 1</p>
     </div>
   );
 };
