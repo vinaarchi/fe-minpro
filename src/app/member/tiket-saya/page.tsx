8d0b1f836d7c6f5b7d349fbe6290dc7b8ba740ef
kusumaningrum-code <kusumaningrum416@gmail.com>
2025-01-08 00:33:44 +0700

    tiketsaya

--- src/app/member/tiket-saya/page.tsx
+++ src/app/member/tiket-saya/page.tsx
@@ -1,150 +1,147 @@
+"use client";
 
-  "use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { format } from "date-fns";
+import Link from "next/link";
+import {
+  FaCalendar,
+  FaMapMarkerAlt,
+  FaClock,
+  FaTicketAlt,
+} from "react-icons/fa";
+import AuthGuard from "@/guard/AuthGuard";
+import { Card, CardHeader } from "@/components/ui/card";
 
-  import { useEffect, useState } from "react";
-  import axios from "axios";
-  import { format } from "date-fns";
-  import Link from "next/link";
-  import {
-    FaCalendar,
-    FaMapMarkerAlt,
-    FaClock,
-    FaTicketAlt,
-  } from "react-icons/fa";
-  import AuthGuard from "@/guard/AuthGuard";
-  import { Card, CardHeader } from "@/components/ui/card";
+interface DiscountCoupon {
+  id: number;
+  code: string;
+  discount: number;
+  expirationDate: string;
+}
 
-  interface DiscountCoupon {
-    id: number;
-    code: string;
-    discount: number;
-    expirationDate: string;
-  }
-
-  interface Transaction {
-    id: number;
-    ticketId: number;
-    finalPrice: number;
-    status: string;
-    createdAt: string;
-    ticket: {
-      ticketName: string;
-      event: {
-        name: string;
-        date: string;
-        time: string;
-        location: string;
-        image: string;
-      };
+interface Transaction {
+  id: number;
+  ticketId: number;
+  finalPrice: number;
+  status: string;
+  createdAt: string;
+  ticket: {
+    ticketName: string;
+    eventId: number;
+    event: {
+      name: string;
+      date: string;
+      time: string;
+      location: string;
+      image: string;
     };
+  };
+}
+
+export default function TicketSaya() {
+  const [tickets, setTickets] = useState<Transaction[]>([]);
+  const [discountCoupons, setDiscountCoupons] = useState<DiscountCoupon[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("userId");
+    if (storedUserId) {
+      setUserId(storedUserId);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const fetchTickets = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3232/transactions/user/${userId}`
+        );
+        setTickets(response.data);
+
+        const couponResponse = await axios.get(
+          `http://localhost:3232/user/${userId}/discount-coupon`
+        );
+        setDiscountCoupons(couponResponse.data.result);
+      } catch (err) {
+        console.log("Failed to fetch tickets:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTickets();
+  }, [userId]);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+      </div>
+    );
   }
 
-  export default function TicketSaya() {
-    const [tickets, setTickets] = useState<Transaction[]>([]);
-    const [discountCoupons, setDiscountCoupons] = useState<DiscountCoupon[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [userId, setUserId] = useState<string | null>(null);
+  return (
+    <AuthGuard allowedRoles={["CUSTOMER"]}>
+      <div className="max-w-7xl mx-auto p-6">
+        <div>
+          <h1 className="text-3xl font-bold mb-8">Coupon Available</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+            {discountCoupons.length > 0 ? (
+              discountCoupons.map((coupon) => (
+                <div
+                  key={coupon.id}
+                  className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow"
+                >
+                  <div className="p-6">
+                    <h3 className="text-xl font-semibold mb-2">
+                      Coupon Code: {coupon.code}
+                    </h3>
+                    <p> Discount: {coupon.discount}%</p>
 
-    useEffect(() => {
-      const storedUserId = localStorage.getItem("userId");
-      if (storedUserId) {
-        setUserId(storedUserId);
-      }
-    }, []);
-
-    useEffect(() => {
-      if (!userId) return;
-
-      const fetchTickets = async () => {
-        try {
-          const response = await axios.get(
-            `http://localhost:3232/transactions/user/${userId}`
-          );
-          setTickets(response.data);
-
-          const couponResponse = await axios.get(
-            `http://localhost:3232/user/${userId}/discount-coupon`
-          );
-          setDiscountCoupons(couponResponse.data.result);
-        } catch (err) {
-          console.log("Failed to fetch tickets:", err);
-        } finally {
-          setLoading(false);
-        }
-      };
-
-      fetchTickets();
-    }, [userId]);
-
-    if (loading) {
-      return (
-        <div className="min-h-screen flex items-center justify-center">
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
-        </div>
-      );
-    }
-
-    
-
-    return (
-      <AuthGuard allowedRoles={["CUSTOMER"]}>
-        <div className="max-w-7xl mx-auto p-6">
-          <div>
-            <h1 className="text-3xl font-bold mb-8">Coupon Available</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-              {discountCoupons.length > 0 ? (
-                discountCoupons.map((coupon) => (
-                  <div
-                    key={coupon.id}
-                    className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow"
-                  >
-                    <div className="p-6">
-                      <h3 className="text-xl font-semibold mb-2">
-                        Coupon Code: {coupon.code}
-                      </h3>
-                      <p> Discount: {coupon.discount}%</p>
-
-                      <div className="space-y-2 text-gray-600">
-                        <div className="flex items-center">
-                          <FaCalendar className="mr-2" />
-                          <span>
-                            Expiration Date:{" "}
-                            {format(
-                              new Date(coupon.expirationDate),
-                              "dd MMMM yyyy"
-                            )}
-                          </span>
-                        </div>
+                    <div className="space-y-2 text-gray-600">
+                      <div className="flex items-center">
+                        <FaCalendar className="mr-2" />
+                        <span>
+                          Expiration Date:{" "}
+                          {format(
+                            new Date(coupon.expirationDate),
+                            "dd MMMM yyyy"
+                          )}
+                        </span>
                       </div>
                     </div>
                   </div>
-                ))
-              ) : (
-                <p>No Active Discount Coupon Available</p>
-              )}
-            </div>
+                </div>
+              ))
+            ) : (
+              <p>No Active Discount Coupon Available</p>
+            )}
           </div>
+        </div>
 
-          <div>
-            <h1 className="text-3xl font-bold mb-8">My Tickets</h1>
-              {tickets.length === 0 ? (
-                <div className="min-h-screen flex flex-col items-center justify-center p-4">
-                <FaTicketAlt className="text-4xl text-gray-400 mb-4" />
-                <h2 className="text-2xl font-semibold text-gray-700 mb-2">
-                  No Tickets Found
-                </h2>
-                <p className="text-gray-500 mb-4">
-                  You haven't purchased any tickets yet.
-                </p>
-                <Link
-                  href="/"
-                  className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors"
-                >
-                  Browse Events
-                </Link>
-              </div>
-              ) : (
-
+        <div>
+          <h1 className="text-3xl font-bold mb-8">My Tickets</h1>
+          {tickets.length === 0 ? (
+            <div className="min-h-screen flex flex-col items-center justify-center p-4">
+              <FaTicketAlt className="text-4xl text-gray-400 mb-4" />
+              <h2 className="text-2xl font-semibold text-gray-700 mb-2">
+                No Tickets Found
+              </h2>
+              <p className="text-gray-500 mb-4">
+                You haven't purchased any tickets yet.
+              </p>
+              <Link
+                href="/"
+                className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors"
+              >
+                Browse Events
+              </Link>
+            </div>
+          ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
               {tickets.map((transaction) => (
                 <div
@@ -153,7 +150,9 @@
                 >
                   <div className="relative h-48">
                     <img
-                      src={transaction.ticket.event.image || "/default-event.jpg"}
+                      src={
+                        transaction.ticket.event.image || "/default-event.jpg"
+                      }
                       alt={transaction.ticket.event.name}
                       className="w-full h-full object-cover"
                     />
@@ -215,9 +214,9 @@
                 </div>
               ))}
             </div>
-              )}
-          </div>
+          )}
         </div>
-      </AuthGuard>
-    );
-  }
+      </div>
+    </AuthGuard>
+  );
+}
